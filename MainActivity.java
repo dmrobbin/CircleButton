dbb88c4893cf7150a0dcedfc02c8891928504fec
dmrobbin <dmrobbin@mail.sfsu.edu>
2016-07-02 14:04:18 -0700

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -2,21 +2,15 @@ package com.example.xross.shapesapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.widget.TextView;
 import android.graphics.Bitmap;
-import android.support.v7.app.ActionBar;
-import android.view.LayoutInflater;
-import android.graphics.Canvas;
 import android.graphics.BitmapFactory;
-import android.graphics.Bitmap;
 import android.widget.RelativeLayout;
 import android.widget.ImageView;
+import android.content.Context;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -29,30 +23,59 @@ public class MainActivity extends AppCompatActivity {
     Shape drawView;
     Paint paint;
     Bitmap bitmap;
+    Button circleButton;
+    Button rectButton;
+    Shape[] shapes;
+    int numberOfCircles;
+    int numberOfRectanlges;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        text = (TextView) findViewById(R.id.textView);
-        text.setText("SJL");
+        text = (TextView) findViewById(R.id.textView);;
         shape1 = ShapeFactory.getShape(this,"Circle");
         shape2 = ShapeFactory.getShape(this,"Rectangle");
-        /*
+
+        drawView = new Circle(this);
         canvas = (RelativeLayout) findViewById(R.id.canvasLayout);
-        canvas.addView(drawView);
-        Canvas imageCanvas = new Canvas();
-        */
+        shapes = new Shape[10];
+
+        circleButton = (Button) findViewById(R.id.cButton);
+        rectButton = (Button) findViewById(R.id.rectangleButton);
+        final Context context = this.getApplicationContext();
+
 
         text.setText(shape1.getShapeType()+ " " + shape2.getShapeType());
+
+
+        circleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) { adjustShapesAlpha();
+
+                    shape1 = ShapeFactory.getShape(context,"Circle");;
+
+            canvas.addView(shape1); updateShapesCount();
+        } });
+
+
+
+        rectButton.setOnClickListener(new View.OnClickListener() { @Override
+        public void onClick(View v) { adjustShapesAlpha();
+
+                shape1 =ShapeFactory.getShape(context,"Rectangle");;
+
+            canvas.addView(shape1); updateShapesCount();
+        } });
     }
 
-    void adjustShapeAlpha(){
+    void adjustShapesAlpha(){
 
     }
 
-    void updateShapeCount(){
+    void updateShapesCount(){
+        text.setText(numberOfRectanlges+ " Rectangles" +numberOfCircles+ " Circles");
 
     }
 }
